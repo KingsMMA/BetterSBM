5c991f37950ebe1f1b606e89529ef5b6b4b5cbc9
KingsMMA <adking08@hotmail.com>
2023-12-01 02:10:59 +1000

    Get transcripts when messages are loaded
    Currently just prints to console

--- src/BetterSBM/index.js
+++ src/BetterSBM/index.js
@@ -6,17 +6,39 @@
  */
 module.exports = (Plugin, Library) => {
 
-    const {Logger} = Library;
-    
+    const {Patcher, Logger} = Library;
+    const RenderFields = BdApi.Webpack.getByPrototypeKeys("renderFields", "getMaxWidth").prototype;
+
     return class extends Plugin {
 
         onStart() {
+            if (window.bsbmCache === undefined) window.bsbmCache = {
+                tickets: {}
+            }
+
+            window.unpatchTicketViewer = Patcher.after(RenderFields, "renderFields", this.loadEmbed);
+
             Logger.info("Plugin enabled!");
         }
 
         onStop() {
+            window.unpatchTicketViewer();
+
             Logger.info("Plugin disabled!");
         }
+
+        loadEmbed(val, args, ret) {
+            if (val.props === null) return;
+            if (val.props.embed === null) return;
+
+            let fields = val.props.embed.fields;
+            for (const field of fields) {
+                if (field.rawName !== "Transcript:") continue;
+                const transcript = field.rawValue.split("(")[1].split(")")[0];
+                console.log(transcript);
+            }
+        }
+
     };
 
 };
